185483a45fe1cc8a40435676e5904813a40a8638
alvaro-78 <alvaro78an@gmail.com>
2021-12-09 19:35:06 +0100

    Checking get method

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Para inicializar el proyecto en terminal:
 - `npm install`, para instalar las dependecias
 - `npm run build` para transpilar el código TS en JS
 - `npm run start` para levantar el servidor de express
+
+### Pasos que he seguido:
+
+- Crear el scaffolding de TS
+- Crear el Servidor de node con express
+- Montar las Interfaces según los tipos que obtenemos de el json que vamos a consumir
+-
--- api/dist/index.js
+++ api/dist/index.js
@@ -1,3 +1,9 @@
 "use strict";
-const clubs = require('../data/clubs.json');
-console.log(clubs);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const server_1 = __importDefault(require("./server"));
+(0, server_1.default)().listen(3000, () => {
+    console.log('Ya estamos escuchando');
+});
